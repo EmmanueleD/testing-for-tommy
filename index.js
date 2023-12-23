a031166c7fe95ef476978ae85d7a89b4d962e411
emmanueledurante <emmanuele.durante@martechrevolution.com>
2023-12-23 19:25:58 -0300

    fix 1.04

--- index.js
+++ index.js
@@ -11,7 +11,4 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+module.exports = app; // Export your app instead of listening to a port
