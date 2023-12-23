74aab547da59ab07ebffbd5aee71d5de7cf57a49
emmanueledurante <emmanuele.durante@martechrevolution.com>
2023-12-23 20:21:03 -0300

    fix 1.09

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
 });
 app.use(cors());
 app.use(express.json());
-app.use("/api", routes);
+app.use("/", routes);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
